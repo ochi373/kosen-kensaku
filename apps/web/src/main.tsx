2e9d59b1be824247657cee5c24580d1bffd4e644
Ueyama Rintaro <kindled0kindred@icloud.com>
2026-05-25 21:44:04 +0900

    feat: password auth-like

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,12 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "@workspace/ui/globals.css";
-import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 import { BrowserRouter, Route, Routes } from "react-router";
 import { SelfTestPage } from "./self-test/page";
 import { ResultPage } from "./result/page";
 import { Layout } from "./layout";
+
+/** ===================================
+ * 疑似的なパスワード保護をかけていますが
+ * 非常に脆弱なため、機密情報を含めないでください。
+ *  =================================== */
+let __unsafe__password = prompt("Enter the password:");
+if (__unsafe__password !== "password") {
+	alert("Incorrect password. The application will now exit.");
+	throw new Error("Incorrect password");
+}
+/** =================================== */
 
 const root = document.getElementById("root");
 if (root == null) throw new Error();
