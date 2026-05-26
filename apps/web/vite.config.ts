361d6ee680aaab6643b56b92aa6c34ec67cbcaf8
Ueyama Rintaro <kindled0kindred@icloud.com>
2026-05-26 21:47:39 +0900

    fix: マッチ条件

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,7 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,5 +14,8 @@ export default defineConfig({
 	},
 	build: {
 		outDir: "../../docs",
-	}
+	},
+	test: {
+    	includeSource: ['src/**/*.{js,ts}'], 
+	},
 });
